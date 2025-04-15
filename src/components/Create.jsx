d6ad2c2eef9eae80cfeb4c5bd8b28c7db19e479c
Iyana Garry <it.garry@gmail.com>
2025-04-14 20:09:54 -0400

    Edited the 'for' attributes in Create.jsx.

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -7,15 +7,15 @@ function Create() {
                     <div className="create_form">
                         <form>
                             <div className="create_title">
-                                <label for="comic_title">Title:</label>
+                                <label htmlFor="comic_title">Title:</label>
                                 <input type="text" id="comic_title" name="comic_title" value="Title" />
                             </div>
                             <div className="create_author">
-                                <label for="comic_author">Author:</label>
+                                <label htmlFor="comic_author">Author:</label>
                                 <input type="text" id="comic_author" name="comic_author" value="Author" />    
                             </div>
                             <div className="select_publisher">
-                                <label for="comic_publisher">Publisher:</label>
+                                <label htmlFor="comic_publisher">Publisher:</label>
                                 <select id="comic_publisher" name="comic_publisher">
                                     <option value="boom_box">BOOM! Box</option>
                                     <option value="dc_comics">DC Comics</option>
@@ -30,16 +30,16 @@ function Create() {
                                 </select>
                             </div>
                             <div className="create_genre">
-                                <label for="comic_genre">Genre:</label><input type="text" id="comic_genre" name="comic_genre" value="Genre" />
+                                <label htmlFor="comic_genre">Genre:</label><input type="text" id="comic_genre" name="comic_genre" value="Genre" />
                             </div>
                             <div className="create_pages">
-                                <label for="comic_pages">Number of pages:</label><input type="text" id="comic_pages" name="comic_pages" value="Number of pages" />
+                                <label htmlFor="comic_pages">Number of pages:</label><input type="text" id="comic_pages" name="comic_pages" value="Number of pages" />
                             </div>
                             <div className="create_rating">
-                                <label for="comic_rating">Rating:</label><input type="text" id="comic_rating" name="comic_rating" size="4" />
+                                <label htmlFor="comic_rating">Rating:</label><input type="text" id="comic_rating" name="comic_rating" size="4" />
                             </div>
                             <div className="create_synopsis">
-                                <label for="comic_synopsis">Synopsis:</label><textarea id="comic_synopsis" name="comic_synopsis">Synopsis</textarea>
+                                <label htmlFor="comic_synopsis">Synopsis:</label><textarea id="comic_synopsis" name="comic_synopsis">Synopsis</textarea>
                             </div>
                             
                             <input type="submit" value="Submit" />
